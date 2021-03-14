3b42c4315c9cb43f37b9fc05aec6b1f72b288fbe
xiongchenyu <xiongchenyu@bigo.sg>
2021-03-14 21:35:54 +0800

    add ignore

--- main.cpp
+++ main.cpp
@@ -9,7 +9,9 @@
 #include <map>
 #include <memory>
 #include <numeric>
+#include <ostream>
 #include <string>
+#include <tuple>
 #include <vector>
 
 #include "ac.h"
@@ -24,21 +26,19 @@ using namespace std;
 
 auto main(int argc, char* args[]) -> int {
     // demo_perm("hate");
-    cout << args[1];
 
-    auto a = vector<string>{"aa"};
-    //demo_thread_local();
+    // demo_thread_local();
     // demo_implicit();
     // map<int, int> m;
     // m[0] = m.size();
-    // cout << R"***("quoted string containing the usual''()))))(((( ""<>d{}\n\rterminator (")")***"
-    //      << "\n"
-    //      << sizeof(int) << "\n"
-    //      << sizeof(char) << "\n"
-    //      << m[0] << "\n"
-    //      << endl;
-    //
-    int z = 12;
+    // cout
+    //     << R"***("quoted string containing the usual''()))))(((( ""<>d{}\n\rterminator (")")***"
+    //     << "\n"
+    //     << sizeof(int) << "\n"
+    //     << sizeof(char) << "\n"
+    //     << m[0] << "\n"
+    //     << endl;
+
     // string X = "ABCBDAB";
     // string Y = "BDCABA";
     // cout << lcs(X, Y, 4, 4) << "\n";
@@ -58,20 +58,39 @@ auto main(int argc, char* args[]) -> int {
     // destory(root);               //释放动态申请内存
     //                              // this_thread::sleep_for(chrono::seconds(10));
     //
-    std::vector<int> v = {7, 1, 4, 0, -1};
+    // std::vector<int> v = {7, 1, 4, 0, -1};
 
-    std::ranges::sort(v);
+    // std::ranges::sort(v);
 
-    DEBUG::debug(z, v);
-    std::string s1 = "Test";
+    // DEBUG::debug(v);
+    // std::string s1 = "Test";
 
-    std::string& r1 = s1;// 错误：不能绑定到左值
+    // std::string& r1 = s1;// 错误：不能绑定到左值
 
-    const std::string& r2 = s1;// 可行：到常值的左值引用延长生存期
-    //  r2 += "Test";                    // 错误：不能通过到常值的引用修改
+    // const std::string& r2 = s1;// 可行：到常值的左值引用延长生存期
+    // //  r2 += "Test";                    // 错误：不能通过到常值的引用修改
 
-    std::string&& r3 = s1 + s1;// 可行：右值引用延长生存期
-    r3 += "Test";// 可行：能通过到非常值的右值引用修改
-    DEBUG::debug(s1, r2, r3);
+    // std::string&& r3 = s1 + s1;// 可行：右值引用延长生存期
+    // r3 += "Test";// 可行：能通过到非常值的右值引用修改
+    // //
+    const char* eee = "fsdaf";
+
+    cerr << eee << endl;
+
+    while (*eee != '\0') {
+        cout << *eee << endl;
+        ++eee;
+    }
+    cout << "test" << endl;
+    auto n = make_tuple(1, 'c', "fsadf");
+    int z = 12;
+    string s = to_string(z);
+    int m = stoi(s);
+    auto ll = get<2>(n);
+
+    cout << get<0>(n);
+    cout << get<1>(n);
+    cout << get<2>(n);
+
     return 0;
 }
